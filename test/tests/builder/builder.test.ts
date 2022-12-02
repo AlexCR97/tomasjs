f81ea7db2a4ac8a5b77432362e539b087f785490
viex <carp_97@outlook.com>
2022-12-02 09:18:45 -0600

    Moved lib folders 1 directory up

--- test/tests/builder/builder.test.ts
+++
@@ -1,9 +0,0 @@
-import { describe, expect, it } from "@jest/globals";
-import { AppBuilder } from "@thomas/builder";
-
-describe("AppBuilder", () => {
-  it("ThomasJS tests work", () => {
-    const app = new AppBuilder();
-    expect(app).toBeTruthy();
-  });
-});
--- test/tests/main.test.ts
+++
@@ -1,9 +0,0 @@
-import { greet } from "@/utils/greet";
-import { describe, expect, it } from "@jest/globals";
-
-describe("@thomas/test", () => {
-  it("ThomasJS tests work", () => {
-    const message = greet("ThomasJS");
-    expect(message).toEqual("Hello ThomasJS!");
-  });
-});
