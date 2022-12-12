be65db215bd1b6e5b9cc0caf9e5df4be94916f18
viex <carp_97@outlook.com>
2022-12-12 09:23:44 -0600

    Added test to inject MongoOrm

--- test/mikro-orm/mongodb/mikro-orm-mongodb.test.ts
+++ test/mikro-orm/mongodb/mikro-orm-mongodb.test.ts
@@ -3,10 +3,14 @@ import { afterEach, beforeEach, describe, it } from "@jest/globals";
 import { tryCloseServerAsync } from "../../utils/server";
 import { tick } from "../../utils/time";
 import { AppBuilder } from "../../../src/builder";
-import { MongoInstance, MongoSetupFactory } from "../../../src/mikro-orm/mongodb";
-
+import { HttpContext, StatusCodes } from "../../../src/core";
+import { Endpoint } from "../../../src/endpoints";
+import { MongoInstance, MongoOrm, MongoSetupFactory } from "../../../src/mikro-orm/mongodb";
+import { PlainTextResponse } from "../../../src/responses";
 import { Entity, PrimaryKey, Property, SerializedPrimaryKey } from "@mikro-orm/core";
 import { ObjectId } from "@mikro-orm/mongodb";
+import { injectable } from "tsyringe";
+import fetch from "node-fetch";
 
 @Entity()
 class User {
@@ -25,9 +29,12 @@ class User {
 
 describe("MikroORM - MongoDB", () => {
   const port = 3035;
-  // const serverAddress = `http://localhost:${port}`;
+  const serverAddress = `http://localhost:${port}`;
   const serverTeardownOffsetMilliseconds = 50;
   let server: any; // TODO Set http.Server type
+
+  const connectionString = "mongodb://127.0.0.1:27017";
+  const database = "tomasjs-tests";
 
   beforeEach(async () => {
     await tick(serverTeardownOffsetMilliseconds);
@@ -43,9 +50,6 @@ describe("MikroORM - MongoDB", () => {
 
   it(`Can connect via ${MongoSetupFactory.name}`, async () => {
     // Arrange
-    const connectionString = "mongodb://127.0.0.1:27017";
-    const database = "tomasjs-tests";
-
     server = await new AppBuilder()
       .register(
         new MongoSetupFactory({
@@ -55,5 +59,45 @@ describe("MikroORM - MongoDB", () => {
         })
       )
       .buildAsync(port);
+  });
+
+  it(`Can inject ${MongoOrm.name}`, async () => {
+    // Arrange
+    const successMessage = "MongoOrm works!";
+    const resourcePath = "users";
+
+    @injectable()
+    class CreateUserEndpoint extends Endpoint {
+      constructor(private readonly mongo: MongoOrm) {
+        super();
+        this.method("post").path(`/${resourcePath}`);
+      }
+      handle(context: HttpContext) {
+        const message = this.mongo?.em !== undefined ? successMessage : ":(";
+        return new PlainTextResponse(message);
+      }
+    }
+
+    server = await new AppBuilder()
+      .register(
+        new MongoSetupFactory({
+          clientUrl: connectionString,
+          dbName: database,
+          entities: [User],
+        })
+      )
+      .useEndpoint(CreateUserEndpoint)
+      .buildAsync(port);
+
+    // Act
+    const response = await fetch(`${serverAddress}/${resourcePath}`, {
+      method: "post",
+      body: "",
+      headers: { "Content-Type": "text/plain" },
+    });
+
+    // Assert
+    expect(response.status).toBe(StatusCodes.ok);
+    expect(response.text()).resolves.toEqual(successMessage);
   });
 });
