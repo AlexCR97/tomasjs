f2b4dc1aa46444cf52f9c30dab200e9a6d0276d9
viex <carp_97@outlook.com>
2023-02-08 14:02:54 -0600

    - Updated package version. - Added excludes for /examples and test files. - Improved exporting of modules.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,28 @@
-/* #region Container namespace */
+export { AppBuilder } from "./builder";
+export { ContainerBuilder } from "./builder";
+export { ContainerSetup, ContainerSetupFactory } from "./builder";
+export { ContainerTeardown, ContainerTeardownFactory } from "./builder";
 
 export { inject, injectable } from "./container";
 
-/* #endregion */
+export { HttpContext, StatusCodes, UserContext } from "./core";
 
-// TODO Add exports for other namespaces
+export {
+  NotImplementedError,
+  RequiredArgumentError,
+  StatusCodeError,
+  TomasError,
+} from "./core/errors";
+
+export { BaseResponse, JsonResponse, PlainTextResponse } from "./responses";
+
+export {
+  BadRequestResponse,
+  CreatedResponse,
+  ForbiddenResponse,
+  NoContentResponse,
+  NotFoundResponse,
+  OkResponse,
+  StatusCodeResponse,
+  UnauthorizedResponse,
+} from "./responses/status-codes";
