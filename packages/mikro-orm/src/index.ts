081fb84a40402e59efbb53b13a6dd02f0aeea2f9
viex <carp_97@outlook.com>
2023-04-28 18:46:51 -0600

    - Updated public API. - Removed legacy tests.

--- packages/mikro-orm/src/index.ts
+++ packages/mikro-orm/src/index.ts
@@ -1,5 +1,8 @@
 export { injectMikroOrm } from "./@injectMikroOrm";
 export { DatabaseDriver } from "./DatabaseDriver";
 export { DisposeMikroOrm } from "./DisposeMikroOrm";
+export { MikroOrmOptions } from "./MikroOrmOptions";
+export { OnConnectedFunction } from "./OnConnectedFunction";
+export { OnOrmBootstrappedFunction } from "./OnOrmBootstrappedFunction";
 export { UseMikroOrm } from "./UseMikroOrm";
 export { UseRepositories } from "./UseRepositories";
