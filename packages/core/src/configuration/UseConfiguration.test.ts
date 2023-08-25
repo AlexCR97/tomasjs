f48d1638f343932618fdebd71965a1d9e3a7b8bc
viex <carp_97@outlook.com>
2023-08-24 19:16:48 -0600

    feat(core): add json configuration

--- packages/core/src/configuration/UseConfiguration.test.ts
+++ packages/core/src/configuration/UseConfiguration.test.ts
@@ -1,11 +1,12 @@
 import "reflect-metadata";
 import { it, describe, expect } from "@jest/globals";
-import { ServiceContainerBuilder } from "..";
 import { UseConfiguration } from "./UseConfiguration";
 import { configurationToken } from "./configurationToken";
+import { ServiceContainerBuilder, inject, injectable } from "@/container";
+import { Configuration } from "./Configuration";
 
 describe("UseConfiguration", () => {
-  it("Can bind a configuration object", async () => {
+  it("Can bind a configuration object from a .env source", async () => {
     interface MyConfiguration {
       stringProp: string;
       numberProp: number;
@@ -33,7 +34,60 @@ describe("UseConfiguration", () => {
       )
       .buildServiceProviderAsync();
 
+    const configuration = services.get<Configuration<MyConfiguration>>(configurationToken);
+
+    expect(configuration).toBeTruthy();
+    expect(typeof configuration.stringProp === "string").toBeTruthy();
+    expect(typeof configuration.numberProp === "number").toBeTruthy();
+    expect(typeof configuration.booleanProp === "boolean").toBeTruthy();
+  });
+
+  it("Can bind a configuration object from a json source", async () => {
+    interface MyConfiguration {
+      stringProp: string;
+      numberProp: number;
+      booleanProp: boolean;
+    }
+
+    const services = await new ServiceContainerBuilder()
+      .setup(
+        new UseConfiguration<MyConfiguration>({
+          source: "json",
+        })
+      )
+      .buildServiceProviderAsync();
+
     const configuration = services.get<MyConfiguration>(configurationToken);
+
+    expect(configuration).toBeTruthy();
+    expect(typeof configuration.stringProp === "string").toBeTruthy();
+    expect(typeof configuration.numberProp === "number").toBeTruthy();
+    expect(typeof configuration.booleanProp === "boolean").toBeTruthy();
+  });
+
+  it("Can inject a configuration object", async () => {
+    interface MyConfiguration {
+      stringProp: string;
+      numberProp: number;
+      booleanProp: boolean;
+    }
+
+    @injectable()
+    class MyService {
+      constructor(@inject(configurationToken) readonly config: Configuration<MyConfiguration>) {}
+    }
+
+    const services = await new ServiceContainerBuilder()
+      .addClass(MyService)
+      .setup(
+        new UseConfiguration<MyConfiguration>({
+          source: "json",
+        })
+      )
+      .buildServiceProviderAsync();
+
+    const myService = services.get(MyService);
+    const configuration = myService.config;
 
     expect(configuration).toBeTruthy();
     expect(typeof configuration.stringProp === "string").toBeTruthy();
--- packages/core/src/configuration/UseConfiguration.ts
+++ packages/core/src/configuration/UseConfiguration.ts
@@ -1,37 +1,84 @@
 import { parse } from "dotenv";
+import { readFileSync } from "fs";
 import { ContainerSetupFactory, ContainerSetupFunction } from "@/container";
 import { TomasError } from "@/errors";
+import { ConfigurationSource } from "./ConfigurationSource";
+import { DotenvTransform } from "./DotenvConfiguration";
 import { KeyConfiguration } from "./KeyConfiguration";
 import { configurationToken } from "./configurationToken";
-import { DotenvTransform } from "./DotenvConfiguration";
-import { readFileSync } from "fs";
+import { ConfigurationSourceError } from "./ConfigurationSourceError";
 
 export type UseConfigurationOptions<T extends object> = {
+  source?: ConfigurationSource;
   path?: string;
   keyConfigs?: KeyConfiguration<T>[];
 };
 
 export class UseConfiguration<T extends object> implements ContainerSetupFactory {
+  private readonly defaultSource: ConfigurationSource = ".env";
+  private readonly defaultEnvPath = ".env";
+  private readonly defaultJsonPath = "app.config.json";
+
   constructor(private readonly options: UseConfigurationOptions<T>) {}
 
+  private get source(): ConfigurationSource {
+    return this.options.source ?? this.defaultSource;
+  }
+
   private get path(): string {
-    return this.options.path ?? ".env";
+    if (this.options.path !== undefined) {
+      return this.options.path;
+    }
+
+    if (this.source === ".env") {
+      return this.defaultEnvPath;
+    }
+
+    if (this.source === "json") {
+      return this.defaultJsonPath;
+    }
+
+    throw new ConfigurationSourceError(this.source);
   }
 
   create(): ContainerSetupFunction {
     return (container) => {
-      const envFile = this.readFile(this.path);
-      const dotenvParseOutput = parse(envFile);
-      const configurationInstance = new DotenvTransform(this.options.keyConfigs).transform(
-        dotenvParseOutput
-      );
+      const configurationInstance = this.buildConfigurationInstance();
       container.addInstance(configurationInstance, configurationToken);
     };
   }
 
-  private readFile(path: string): Buffer {
+  private buildConfigurationInstance(): Readonly<T> {
+    if (this.source === ".env") {
+      return this.buildEnvConfigurationInstance();
+    }
+
+    if (this.source === "json") {
+      return this.buildJsonConfigurationInstance();
+    }
+
+    throw new ConfigurationSourceError(this.source);
+  }
+
+  private buildEnvConfigurationInstance(): Readonly<T> {
+    const envFileBuffer = this.readFile(this.path);
+    const dotenvParseOutput = parse(envFileBuffer);
+    return new DotenvTransform(this.options.keyConfigs).transform(dotenvParseOutput);
+  }
+
+  private buildJsonConfigurationInstance(): Readonly<T> {
+    const jsonContent = this.readFile(this.path, "utf-8");
+
+    if (typeof jsonContent !== "string") {
+      throw new TomasError("The JSON content should be a string");
+    }
+
+    return JSON.parse(jsonContent);
+  }
+
+  private readFile(path: string, encoding?: BufferEncoding) {
     try {
-      return readFileSync(path);
+      return readFileSync(path, encoding);
     } catch (err) {
       throw new TomasError(`Could not find file at path "${path}"`, {
         data: { path },
