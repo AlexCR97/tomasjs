b97819a11f2d3d6c557b22a5bcd8f091329ac356
viex <carp_97@outlook.com>
2024-07-31 19:07:39 -0600

    feat(core): export http modules

---
+++ packages/core/src/http2/index.ts
@@ -0,0 +1,25 @@
+export { HttpClient, HttpRequestOptions, IHttpClient } from "./HttpClient";
+export {
+  HTTP_CONTENT_TYPES,
+  HtmlContent,
+  HttpContentFactory,
+  HttpContentType,
+  IHttpContent,
+  IHttpContentFactory,
+  JsonContent,
+  JsonRecord,
+  PlainTextContent,
+  RawContent,
+} from "./HttpContent";
+export {
+  HttpHeader,
+  HttpHeaderValue,
+  HttpHeaders,
+  IHttpHeaders,
+  PlainHttpHeaders,
+} from "./HttpHeaders";
+export { HTTP_METHODS, HttpMethod } from "./HttpMethod";
+export { HttpRequest, HttpRequestError, IHttpRequest, IHttpRequestBuilder } from "./HttpRequest";
+export { HttpResponse, HttpResponseError, IHttpResponse } from "./HttpResponse";
+export { HTTP_STATUS_CODES, HTTP_STATUS_TEXT } from "./HttpStatus";
+export { JsonResponseError } from "./JsonResponseError";
